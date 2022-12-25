eb7ed6c8a46fb2d367780209040bac79f77d5b68
real_sohel <salmanisohel26@gmail.com>
2022-12-25 20:33:32 +0530

    check user already exist or not

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,23 +5,42 @@ const User = require("../models/User");
 const { body, validationResult } = require('express-validator');
 
 
-// Craete User using POST ..
-router.post('/' ,[
+// Craete User using POST without login.. /api/auth/createuser
+router.post('/createuser' ,[
     body('name', "Your name is too short...").isLength({ min: 3 }),
     body('email', "Enter a valid email").isEmail(),
     body('password', "Your Password must contain atleast 6 characters").isLength({ min: 6 })
-], (req,res)=>{
+], async (req,res)=>{
+
+    // If error occurs, return Bad req.
     const errors = validationResult(req);
     if (!errors.isEmpty()) { // validator wali web se liya 
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    
+
+    try{
+
+    
+    // Check user with same email exist or not.
+    let user = await User.findOne({email : req.body.email});
+
+    if(user){
+        return res.status(400).json({error: "User with same email already exist."});
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        }).then(user => res.json(user))
-        .catch(err=>{console.log(err)
-        res.json({error:"Enter a unique email address", message: err})});
+        })
+    
+    res.json(user);
+    console.log(user);
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Glitched occurs");
+    }
 })
 
 module.exports = router ;
